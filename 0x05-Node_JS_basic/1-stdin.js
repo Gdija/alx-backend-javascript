5baf1d5b2d1ef0b480459c7fb25ac9fb4f7265b0
Kghadi <khadijaghadi00@gmail.com>
2025-01-25 12:55:51 +0000

    execute a program from CLI

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,7 +2,8 @@
  * create a program that that will 
  * be executed through command line
  * */
-console.log("Welcome to ALX, what is your name?\n");
+process.stdout.write("Welcome to ALX, what is your name?\n");
+
 process.stdin.on('data', data =>{
         console.log(`Your name is: ${data.toString()}`);
 });
