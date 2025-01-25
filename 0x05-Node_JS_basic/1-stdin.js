6c4ac331869e23cfe93dd1029c72ec0477326ab5
Kghadi <khadijaghadi00@gmail.com>
2025-01-25 13:33:54 +0000

    fix file

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -5,8 +5,9 @@
 process.stdout.write("Welcome to ALX, what is your name?\n");
 
 process.stdin.on('data', data =>{
-	const name = data.toString().trim();
-        console.log(`Your name is: ${name}`);
+	//const name = data.toString().trim();
+        //console.log(`Your name is: ${name}`);
+	process.stdout.write(`Your name is: ${data}`);
 });
 
 process.stdin.on('end', () =>{
